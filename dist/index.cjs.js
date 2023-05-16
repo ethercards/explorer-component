fb9012a5b31972d236197f617de20c39e208250b
Szatmari Csongi <szatmari.csongi@gmail.com>
2023-05-16 14:59:07 +0300

    css fix

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -18,6 +18,101 @@ var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 var InfiniteScroll__default = /*#__PURE__*/_interopDefaultLegacy(InfiniteScroll);
 var axios__default = /*#__PURE__*/_interopDefaultLegacy(axios);
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
+  }
+
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
+  }
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+        args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+
+      _next(undefined);
+    });
+  };
+}
+
+function _slicedToArray(arr, i) {
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+}
+
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+
+function _iterableToArrayLimit(arr, i) {
+  var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
+
+  if (_i == null) return;
+  var _arr = [];
+  var _n = true;
+  var _d = false;
+
+  var _s, _e;
+
+  try {
+    for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) {
+      _arr.push(_s.value);
+
+      if (i && _arr.length === i) break;
+    }
+  } catch (err) {
+    _d = true;
+    _e = err;
+  } finally {
+    try {
+      if (!_n && _i["return"] != null) _i["return"]();
+    } finally {
+      if (_d) throw _e;
+    }
+  }
+
+  return _arr;
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
 const loadNext = (nfts, ITEMS_PER_PAGE, currentPageRef, setCurrentPage, setCards) => {
   let c = [];
   let end = nfts.length < ITEMS_PER_PAGE ? nfts.length : ITEMS_PER_PAGE;
@@ -135,41 +230,48 @@ function styleInject(css, ref) {
   }
 }
 
-var css_248z = ".layer-image-preview {\r\n  padding-bottom: 20px;\r\n  text-align: center;\r\n}\r\n.infinite-scroll-component__outerdiv {\r\n  max-width: 1200px;\r\n  margin: 0 auto;\r\n}\r\n.infinite-scroll-component {\r\n  overflow-x: hidden !important;\r\n}\r\n\r\n/* card */\r\n.explorer-simple-card-container {\r\n}\r\n.explorer-simple-card {\r\n  display: flex;\r\n  flex-direction: column;\r\n  text-align: left;\r\n  border-radius: 10px;\r\n  overflow: hidden;\r\n  color: #fff;\r\n  cursor: pointer;\r\n}\r\n.explorer-simple-card-trait-container {\r\n  padding: 10px;\r\n  background: linear-gradient(rgb(20, 33, 46), rgb(14, 25, 36));\r\n}\r\n.explorer-simple-card-trait {\r\n  max-width: 30px;\r\n}\r\n.explorer-simple-card-traits {\r\n  margin-top: 10px;\r\n  min-height: 30px;\r\n}\r\n";
+var css_248z = ".layer-image-preview {\r\n  padding-bottom: 20px;\r\n  text-align: center;\r\n}\r\n.infinite-scroll-component__outerdiv {\r\n  max-width: 1200px;\r\n  margin: 0 auto;\r\n}\r\n.infinite-scroll-component {\r\n  overflow-x: hidden !important;\r\n}\r\n\r\n/* card */\r\n.explorer-simple-card-container {\r\n}\r\n.explorer-simple-card {\r\n  display: flex;\r\n  flex-direction: column;\r\n  text-align: left;\r\n  border-radius: 10px;\r\n  overflow: hidden;\r\n  color: #fff;\r\n  cursor: pointer;\r\n}\r\n.explorer-simple-card-trait-container {\r\n  padding: 10px;\r\n  background: linear-gradient(rgb(20, 33, 46), rgb(14, 25, 36));\r\n}\r\n.explorer-simple-card-trait {\r\n  max-width: 30px;\r\n}\r\n.explorer-simple-card-traits {\r\n  margin-top: 10px;\r\n  min-height: 30px;\r\n}\r\n.dust-pool-root{\r\n  max-width: 1140px;\r\n  width: 100%;\r\n  margin: 0 auto;\r\n  margin-top: 30px;\r\n  font-family: poppins;\r\n}\r\n.dust-pool-textbox{\r\ntext-align: center;\r\nmax-width: 600px;\r\nmargin: 0 auto;\r\n}\r\n.pool-subtitle{\r\n  font-size: 30px;\r\n  font-weight: 600;\r\n}\r\n.tab-choose{\r\n  display: flex;\r\n  justify-content: center;\r\n  margin-bottom: 30px;\r\n  text-transform: uppercase;\r\n}\r\n.tab-choose div{\r\n  font-size: 14px;\r\n  font-weight: 500;\r\n  padding: 12px 15px\r\n}\r\n.tab-choose div:hover{\r\n  cursor: pointer;\r\n}\r\n.tab-choose .active-tab{\r\n  background-color: #000;\r\n  color: #fff;\r\n}\r\n.tab-choose div:first-child{\r\n  border: 2px solid #000;\r\n  border-top-left-radius: 10px;\r\n  border-bottom-left-radius: 10px;\r\n}\r\n.tab-choose div:nth-child(2){\r\n  border-top: 2px solid #000;\r\n  border-bottom: 2px solid #000;\r\n}\r\n.tab-choose div:nth-child(3){\r\n  border: 2px solid #000;\r\n  border-top-right-radius: 10px;\r\n  border-bottom-right-radius: 10px;\r\n}\r\n/*classes from bootstrap*/\r\n*{\r\n  box-sizing: border-box;\r\n}\r\n.container {\r\n  width: 100%;\r\n  padding-right: 15px;\r\n  padding-left: 15px;\r\n  margin-right: auto;\r\n  margin-left: auto;\r\n}\r\n.mt-5, .my-5 {\r\n  margin-top: 3rem!important;\r\n}\r\n.row {\r\n  display: flex;\r\n  flex-wrap: wrap;\r\n  margin-right: -15px;\r\n  margin-left: -15px;\r\n}\r\n.col-12 {\r\n  flex: 0 0 100%;\r\n  max-width: 100%;\r\n}\r\n.col-12, .col-lg-6, .col-lg-3, .col-md-6{\r\n  position: relative;\r\n  width: 100%;\r\n  min-height: 1px;\r\n  padding-right: 15px;\r\n  padding-left: 15px;\r\n}\r\n.col-6 {\r\n  flex: 0 0 50%;\r\n  max-width: 50%;\r\n}\r\n.mb-1{\r\n  margin-bottom: 0.25rem!important;\r\n}\r\n.mb-2{\r\n  margin-bottom: 0.5rem!important;\r\n}\r\n.mb-3{\r\n  margin-bottom: 0.75rem!important;\r\n}\r\n.mt-2{\r\n  margin-top: 0.5rem!important;\r\n}\r\n.pb-4{\r\n  padding-bottom: 1.5rem!important;\r\n}\r\n.w-100 {\r\n  width: 100%!important;\r\n}\r\n.text-right{\r\n  text-align: right!important;\r\n}\r\n.dust-pool-card p {\r\n  margin-block-start: 0;\r\n}\r\n.section-divider-img{\r\n  max-height: 35px;\r\n  z-index: 2;\r\n}\r\n@media only screen and (max-width: 500px) {\r\n  .section-divider-img{\r\n    max-height: 25px;\r\n    z-index: 2;\r\n  }\r\n}\r\n@media (min-width: 576px){\r\n.container {\r\n  max-width: 540px;\r\n}\r\n}\r\n@media (max-width: 600px){\r\n  .dust-pool-card .dust-pool-btn{\r\n      position: unset!important;\r\n  }\r\n  }\r\n@media (min-width: 768px){\r\n  .container {\r\n      max-width: 720px;\r\n  }}\r\n  .col-md-6{\r\n      flex: 0 0 50%;\r\n      max-width: 50%;\r\n  }\r\n@media (min-width: 992px){\r\n  .container {\r\n      max-width: 960px;\r\n  }\r\n  .col-lg-6 {\r\n      flex: 0 0 50%;\r\n      max-width: 50%;\r\n  }\r\n  .col-lg-3 {\r\n      flex: 0 0 25%;\r\n      max-width: 25%;\r\n  }\r\n}\r\n@media (min-width: 1200px){\r\n  .container {\r\n      max-width: 1140px;\r\n  }\r\n}\r\n@media only screen and (max-width: 945px) {\r\n  .dust-pool-root {\r\n    max-width: 100%;\r\n  }\r\n}";
 styleInject(css_248z);
 
-const ExplorerCards = _ref => {
-  let {
-    nftList,
-    traitTypes,
-    height,
-    tokenAddres,
-    openseaUrl,
-    etherScanUrl
-  } = _ref;
-  const ITEMS_PER_PAGE = 29;
-  const [cards, setCards] = React.useState([]);
-  const [currentPage, _setCurrentPage] = React.useState(0);
-  const currentPageRef = React.useRef(currentPage);
+var ExplorerCards = function ExplorerCards(_ref) {
+  var nftList = _ref.nftList,
+      traitTypes = _ref.traitTypes,
+      height = _ref.height,
+      tokenAddres = _ref.tokenAddres,
+      openseaUrl = _ref.openseaUrl;
+      _ref.etherScanUrl;
+  var ITEMS_PER_PAGE = 29;
 
-  const setCurrentPage = val => {
+  var _useState = React.useState([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      cards = _useState2[0],
+      setCards = _useState2[1];
+
+  var _useState3 = React.useState(0),
+      _useState4 = _slicedToArray(_useState3, 2),
+      currentPage = _useState4[0],
+      _setCurrentPage = _useState4[1];
+
+  var currentPageRef = React.useRef(currentPage);
+
+  var setCurrentPage = function setCurrentPage(val) {
     currentPageRef.current = val;
 
     _setCurrentPage(val);
   };
 
-  const handleOpenOpensea = id => {
-    window.open(`${openseaUrl}/${tokenAddres}/${id}`);
+  var handleOpenOpensea = function handleOpenOpensea(id) {
+    window.open("".concat(openseaUrl, "/").concat(tokenAddres, "/").concat(id));
   };
 
-  React.useEffect(() => {
+  React.useEffect(function () {
     setCards([]);
     setCurrentPage(0);
     loadNext(nftList, ITEMS_PER_PAGE, currentPageRef, setCurrentPage, setCards);
   }, [nftList]);
 
-  const renderCards = () => {
-    return cards.map((meta, i) => {
+  var renderCards = function renderCards() {
+    return cards.map(function (meta, i) {
       return /*#__PURE__*/React__default["default"].createElement(ExplorerCard, {
         meta: meta,
         traitTypes: traitTypes,
@@ -187,7 +289,9 @@ const ExplorerCards = _ref => {
   }, /*#__PURE__*/React__default["default"].createElement(InfiniteScroll__default["default"], {
     dataLength: cards.length,
     height: height || '100vh',
-    next: () => loadNext(nftList, ITEMS_PER_PAGE, currentPageRef, setCurrentPage, setCards),
+    next: function next() {
+      return loadNext(nftList, ITEMS_PER_PAGE, currentPageRef, setCurrentPage, setCards);
+    },
     pullDownToRefreshThreshold: 500,
     hasMore: currentPageRef.current * ITEMS_PER_PAGE < nftList.length,
     scrollThreshold: "1200px" // scrollableTarget="content-container"
@@ -199,7 +303,7 @@ const ExplorerCards = _ref => {
       }
     }, "Loading...")
   }, /*#__PURE__*/React__default["default"].createElement("div", {
-    className: `row small-gutters px-2 mx-0  `,
+    className: "row small-gutters px-2 mx-0  ",
     style: {
       padding: '10px'
     }
@@ -2486,74 +2590,147 @@ const zoomFetchTokenUris = async (contract, zoom2, address) => {
   }
 };
 
-const useGetNftsList = (chainId, contractAddres, address, rpcUrl) => {
-  const [zoomContract, setZoomContract] = React.useState(null);
-  const [nftList, setNftList] = React.useState([]);
-  const provider = React.useMemo(() => {
+var useGetNftsList = function useGetNftsList(chainId, contractAddres, address, rpcUrl) {
+  var _useState = React.useState(null),
+      _useState2 = _slicedToArray(_useState, 2),
+      zoomContract = _useState2[0],
+      setZoomContract = _useState2[1];
+
+  var _useState3 = React.useState([]),
+      _useState4 = _slicedToArray(_useState3, 2),
+      nftList = _useState4[0],
+      setNftList = _useState4[1];
+
+  var provider = React.useMemo(function () {
     return getProvider(rpcUrl);
   }, [rpcUrl]); //provider
 
-  const tokenContract = React.useMemo(() => new ethers.Contract(contractAddres, tokenABI.abi, provider), [contractAddres, provider]);
-  const fetchedRef = React.useRef(false);
-  React.useEffect(async () => {
-    if (provider) {
-      const galaxisRegistry = getContract(GALAXIS_REGISTRY, GalaxisRegistry.abi, provider, false);
-      let zoomAddress;
+  var tokenContract = React.useMemo(function () {
+    return new ethers.Contract(contractAddres, tokenABI.abi, provider);
+  }, [contractAddres, provider]);
+  var fetchedRef = React.useRef(false);
+  React.useEffect( /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var galaxisRegistry, zoomAddress, contract;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            if (!provider) {
+              _context.next = 7;
+              break;
+            }
 
-      if (galaxisRegistry) {
-        zoomAddress = await galaxisRegistry.getRegistryAddress('ZOOM').catch(e => {
-          console.log('registry error', e);
-        });
-      }
+            galaxisRegistry = getContract(GALAXIS_REGISTRY, GalaxisRegistry.abi, provider, false);
 
-      if (zoomAddress) {
-        let contract = getContract(zoomAddress, ZoomAbi.abi, provider, false);
+            if (!galaxisRegistry) {
+              _context.next = 6;
+              break;
+            }
 
-        if (contract) {
-          setZoomContract(contract);
-        } else {
-          zoomAddress = useZoom2Contract(chainId);
-          setZoomContract(zoomAddress);
+            _context.next = 5;
+            return galaxisRegistry.getRegistryAddress('ZOOM').catch(function (e) {
+              console.log('registry error', e);
+            });
+
+          case 5:
+            zoomAddress = _context.sent;
+
+          case 6:
+            if (zoomAddress) {
+              contract = getContract(zoomAddress, ZoomAbi.abi, provider, false);
+
+              if (contract) {
+                setZoomContract(contract);
+              } else {
+                zoomAddress = useZoom2Contract(chainId);
+                setZoomContract(zoomAddress);
+              }
+            }
+
+          case 7:
+          case "end":
+            return _context.stop();
         }
       }
-    } //setting up the zoom contract;
+    }, _callee);
+  })), [chainId, rpcUrl]);
+  React.useEffect( /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            if (!(fetchedRef.current === false)) {
+              _context2.next = 5;
+              break;
+            }
 
-  }, [chainId, rpcUrl]);
-  React.useEffect(async () => {
-    if (fetchedRef.current === false) {
-      if (zoomContract && tokenContract && address) {
-        await zoomFetchTokenUris(tokenContract, zoomContract, address).then(res => {
-          setNftList(res);
-        }).catch(e => {
-          console.log(e);
-        });
+            if (!(zoomContract && tokenContract && address)) {
+              _context2.next = 4;
+              break;
+            }
+
+            _context2.next = 4;
+            return zoomFetchTokenUris(tokenContract, zoomContract, address).then(function (res) {
+              setNftList(res);
+            }).catch(function (e) {
+              console.log(e);
+            });
+
+          case 4:
+            fetchedRef.current === true;
+
+          case 5:
+          case "end":
+            return _context2.stop();
+        }
       }
-
-      fetchedRef.current === true;
-    }
-  }, [zoomContract, tokenContract, fetchedRef.current, address]);
+    }, _callee2);
+  })), [zoomContract, tokenContract, fetchedRef.current, address]);
   return {
-    nftList
+    nftList: nftList
   };
 };
 
-const ExplorerComponent = _ref => {
-  let {
-    tokenAddres,
-    poolAddress,
-    chainId,
-    rpcUrl,
-    openseaUrl,
-    etherScanUrl
-  } = _ref;
-  const {
-    nftList
-  } = useGetNftsList(chainId, tokenAddres, poolAddress, rpcUrl);
-  const [traitTypes, setTraitTypes] = React.useState(null);
-  React.useEffect(() => {
-    const getTraitTypes = async () => {
-      await axios__default["default"].get('https://cms.galaxis.xyz/trait_types').then(resp => setTraitTypes(resp.data));
-    };
+var ExplorerComponent = function ExplorerComponent(_ref) {
+  var tokenAddres = _ref.tokenAddres,
+      poolAddress = _ref.poolAddress,
+      chainId = _ref.chainId,
+      rpcUrl = _ref.rpcUrl,
+      openseaUrl = _ref.openseaUrl,
+      etherScanUrl = _ref.etherScanUrl;
+
+  var _useGetNftsList = useGetNftsList(chainId, tokenAddres, poolAddress, rpcUrl),
+      nftList = _useGetNftsList.nftList;
+
+  var _useState = React.useState(null),
+      _useState2 = _slicedToArray(_useState, 2),
+      traitTypes = _useState2[0],
+      setTraitTypes = _useState2[1];
+
+  React.useEffect(function () {
+    var getTraitTypes = /*#__PURE__*/function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return axios__default["default"].get('https://cms.galaxis.xyz/trait_types').then(function (resp) {
+                  return setTraitTypes(resp.data);
+                });
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      return function getTraitTypes() {
+        return _ref2.apply(this, arguments);
+      };
+    }();
 
     getTraitTypes();
   }, []);
